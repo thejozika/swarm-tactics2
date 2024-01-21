9e0c6110711a435ab63c6f1e25c7c418e0f58c78
Johannes <johannes@jozika.de>
2024-01-21 13:07:59 +0100

    added canvas

---
+++ swarm-tactics-app/components/GameCanvasTestComponent.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+import GameCanvas from "@/component/GameCanvas";
+
+interface GameCanvasTestComponentProps {
+
+}
+
+const GameCanvasTestComponent: FC<GameCanvasTestComponentProps> = ({}) => {
+    return <></>// <GameCanvas step={1} game={null}>GameCanvasTestComponent</GameCanvas>
+}
+
+export default GameCanvasTestComponent
